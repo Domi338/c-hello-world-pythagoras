a1c355108e1fb38c2855bf3a8c70b4611d702735
Domi338 <domi338@live.de>
2016-03-27 01:04:05 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -14,7 +14,7 @@ using namespace std;
  */
 int main(int argc, char** argv) {
     helloworld hw;
-    cout << hw.givesentence() << endl;
+    cout << hw.sentence << endl;
     cout << "pythagoras from a = 1, b = 2" << endl;
     cout << hw.pythagoras(1.0,2.0) << endl;
     cout << ":P" << endl;
